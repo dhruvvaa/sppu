052d086722a9ad4f988d41dd626f33a279b1526c
Dhruva Shah <dhruva.shah2506@gmail.com>
2025-01-08 22:58:27 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -3,7 +3,7 @@ int main()
 {
     double n1,n2;
     char s;
-    printf("Enter symbol of calculation to be performed.\n1.+:Addition\n2.-:Subtraction\n3.*:Multiplication\n4./:Division\n");
+    printf("Enter symbol of calculation to be performed.\n1.+:Addition\n2.-:Subtraction\n3.*:Multiplication\n4./:Division\n5.%:Modulus\n");
     scanf("%c",&s);
     printf("\nEnter two numbers:");
     scanf("%lf %lf",&n1,&n2);
@@ -16,6 +16,8 @@ int main()
         case '*':printf("\nMultiplication:%lf",n1*n2);
         break;
         case '/':printf("\nDivision:%lf",n1/n2);
+        break;
+        case '%':printf("\nRemainder:%lf",n1%n2);
         break;
         default:printf("\nInvalid Operator.");
         break;
