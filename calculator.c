5af0478b13baf9efdb54cbd2b8aeea8153f53577
Dhruva Shah <dhruva.shah2506@gmail.com>
2025-01-08 22:52:46 +0530

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    double n1,n2;
+    char s;
+    printf("Enter symbol of calculation to be performed.\n1.+:Addition\n2.-:Subtraction\n3.*:Multiplication\n4./:Division\n");
+    scanf("%c",&s);
+    printf("\nEnter two numbers:");
+    scanf("%lf %lf",&n1,&n2);
+    switch(s)
+    {
+        case '+':printf("\nAddition:%lf",n1+n2);
+        break;
+        case '-':printf("\nSubtraction:%lf",n1-n2);
+        break;
+        case '*':printf("\nMultiplication:%lf",n1*n2);
+        break;
+        case '/':printf("\nDivision:%lf",n1/n2);
+        break;
+        default:printf("\nInvalid Operator.");
+        break;
+    }
+    return 0;
+}
